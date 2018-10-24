b314b25909056ac65fadbc2953a6760b83b6ded3
goffle <se.legoff@gmail.com>
2018-10-24 10:05:25 +0200

    add optlog

---
+++ src/elasticsearch/listen/optlog.js
@@ -0,0 +1,91 @@
+var MongoOplog = require("mongo-oplog");
+
+const mongoUrl =
+  "mongodb://oploguser:OFXUsS33xC8sDxrQ@staging-shard-00-00-fkthi.mongodb.net:27017,staging-shard-00-01-fkthi.mongodb.net:27017,staging-shard-00-02-fkthi.mongodb.net:27017/local?ssl=true&replicaSet=Staging-shard-0&authSource=admin";
+
+class OPTLOG {
+  constructor(callback, mongoUrl, collection) {
+    this.callback = callback;
+    this.oplog = MongoOplog(mongoUrl, { ns: `${collection}.*` });
+
+    this.oplog.tail();
+
+    this.oplog.on("op", data => {
+      //   console.log(data);
+    });
+    this.oplog.on("insert", doc => {
+      const obj = convert(doc, "insert");
+      if (this.callback) {
+        this.callback(obj);
+      }
+      // console.log(obj);
+    });
+    this.oplog.on("update", doc => {
+      console.log("DATA", doc);
+      const obj = convert(doc, "update");
+      console.log("DATA", obj);
+      if (this.callback) {
+        this.callback(obj);
+      }
+      // console.log(obj);
+    });
+    this.oplog.on("delete", doc => {
+      const obj = convert(doc, "delete");
+      if (this.callback) {
+        this.callback(obj);
+      }
+    });
+
+    this.oplog.on("error", error => {
+      console.log(error);
+    });
+
+    this.oplog.on("end", () => {
+      console.log("Stream ended");
+    });
+
+    this.oplog.stop(() => {
+      console.log("Server stopped");
+    });
+  }
+
+  // Bind connection to error event (to get notification of connection errors)
+}
+
+module.exports = OPTLOG;
+
+function convert(doc, method) {
+  const arr = doc.ns.split(".");
+  const base = arr[0];
+  const collection = arr[1];
+
+  if (method === "delete") {
+    return {
+      base,
+      collection,
+      method: method,
+      id: doc.o._id
+    };
+  }
+
+  if (method === "insert") {
+    return {
+      base,
+      collection,
+      method: method,
+      id: doc.o._id,
+      data: doc.o
+    };
+  }
+
+  const data = doc.o["$set"];
+  delete data._id;
+
+  return {
+    base,
+    collection,
+    method: method,
+    data,
+    id: doc.o2._id
+  };
+}
