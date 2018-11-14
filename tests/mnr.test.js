48b206c15caff0d733e3472d04dd191a3ab31efd
rap2h <raphaelht@gmail.com>
2018-11-14 15:04:44 +0100

    jest + supertest

---
+++ tests/mnr.test.js
@@ -0,0 +1,16 @@
+const app = require("../src/app");
+const request = require("supertest");
+const mongoose = require("mongoose");
+
+afterAll(() => {
+  mongoose.disconnect();
+});
+
+describe("/GET mnr", () => {
+  test(`It should return mnr`, async () => {
+    const response = await request(app)
+      .get("/mnr")
+      .set("Accept", "application/json")
+      .expect(200);
+  });
+});
